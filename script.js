a5ca7124fe7322a139670d6a3f3f777c210e4f1c
Zhicheng Gong <109425to@gmail.com>
2024-08-01 20:42:08 +0800

    new js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
 document.addEventListener('click', function(){
     var button = document.getElementById('closemessage')
     var box = document.getElementById('messagebox')
