69f7ffbde92420434ff3827bcf2d65997d65a88d
Zhicheng Gong <109425to@gmail.com>
2024-08-13 21:52:11 +0800

    1.2.6

--- sitedata.js
+++ sitedata.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
     const footerver = document.getElementById('ver')
-    footerver.innerHTML = '1.2.5'
-    console.log('版本資訊已更新完成')
+    footerver.innerHTML = '1.2.6'
+    console.log('版本資訊已更新完成，目前為最新版本。')
 })
